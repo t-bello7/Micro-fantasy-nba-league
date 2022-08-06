3b523dd508244f1357dbec9311c1a1badcaf2497
Bello Oluwatomi <bello4aus@gmail.com>
2022-08-06 14:00:36 +0100

    fix linter issues

--- src/__tests__/teamReducer.test.js
+++ src/__tests__/teamReducer.test.js
@@ -1,5 +1,7 @@
 import reducer from '../redux/team/teamSlice';
 
 test('should return the inital state', () => {
-  expect(reducer(undefined, { type: undefined })).toEqual({ data: [], error: null, status: "idle",  dataStat: {} });
+  expect(reducer(undefined, { type: undefined })).toEqual({
+    data: [], error: null, status: "idle", dataStat: {}
+  });
 });
