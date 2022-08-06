27c382cea166be82d47f7065d7f201e109643418
Bello Oluwatomi <bello4aus@gmail.com>
2022-08-06 18:26:05 +0100

    style according to guideline

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,6 +4,7 @@ const Card = ({name, logo}) => {
     return (
     <div className="card__container">
         <img src={logo} alt="team-logo" className='card__logo'/>
+        <img src='./icon/left.svg' className="card__icon" alt="left-icon" />
         <div className="card__data">
             <h2> {name} </h2>
         </div>
